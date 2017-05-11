ac4b3c244c9e56f38eca0fe04d46eb4e291d812f
Aluno <Aluno@045LAB04035>
2017-05-11 19:14:13 -0300

    Criação do projeto.

---
+++ CalculadoraWeb/CalculadoraWeb/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace CalculadoraWeb.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ CalculadoraWeb/CalculadoraWeb/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CalculadoraWeb.Startup))]
+namespace CalculadoraWeb
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
