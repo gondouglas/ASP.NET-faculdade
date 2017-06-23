991e83e2f1fb027fc1bbc8936a6fee34847ba02f
Aluno <Aluno@045LAB04035>
2017-06-22 21:30:44 -0300

    Criação do Wingtip Toys

---
+++ WingtipToys_MVC/BaseModels/Categoria.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseModels
+{
+    public class Categoria
+    {
+        public int CategoriaID { get; set; }
+        [Required]
+        public string Nome { get; set; }
+        [Required]
+        public bool Ativo { get; set; }
+
+    }
+}
---
+++ WingtipToys_MVC/BaseModels/Produto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseModels
+{
+    public class Produto
+    {
+        public int ProdutoID { get; set; }
+        [Required]
+        [StringLength(20)]
+        public string Nome { get; set; }
+        
+        public string Descricao { get; set; }
+        [Required]
+        public bool Ativo { get; set; }
+
+        //------Relacionamentos--------
+        public int CategoriaID { get; set; }
+        
+        public virtual Categoria _Categoria { get; set; }
+
+
+    }
+}
